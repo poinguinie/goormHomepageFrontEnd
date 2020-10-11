9aa03aaf1aba3792a8d74e349d3a1a8e447724a8
poinguinie <wlgh0206@gmail.com>
2020-10-11 11:21:12 +0000

    2020.10.11 ver.0.1 update_2

--- js/index.js
+++ js/index.js
@@ -3,22 +3,23 @@ $(function () {
 	// wait for document ready
 	// build scene
 	new ScrollMagic.Scene({
-		triggerElement: '.trigger',
+		triggerElement: '.trigger1',
 		triggerHook: 0.9, // show, when scrolled 10% into view
-		duration: '100%', // hide 10% before exiting view (80% + 10% from bottom)
+		duration: '150%', // hide 10% before exiting view (80% + 10% from bottom)
 		offset: 30, // move trigger to center of element
 	})
-		.setClassToggle('#index', 'visible') // add class to reveal
+		.setClassToggle('#index1', 'visible') // add class to reveal
 		//.addIndicators() // add indicators (requires plugin)
 		.addTo(controller);
-	
+	/*
 	new ScrollMagic.Scene({
-		triggerElement: '.trigger',
+		triggerElement: '.trigger1',
 		triggerHook: 0.9, // show, when scrolled 10% into view
 		duration: '100%', // hide 10% before exiting view (80% + 10% from bottom)
 		offset: 30, // move trigger to center of element
 	})
 		.setClassToggle('#index2', 'visible') // add class to reveal
-		//.addIndicators() // add indicators (requires plugin)
+		.addIndicators() // add indicators (requires plugin)
 		.addTo(controller);
+	*/
 });
